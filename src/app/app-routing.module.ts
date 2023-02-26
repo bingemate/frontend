c03ce3c2d2d5f9fdb7fec20a1c619d2acedd6d71
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-02-26 18:58:20 +0100

    ng lint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   {
     path: 'welcome',
-    loadChildren: () =>
-      import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
+    loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule),
   },
 ];
 
