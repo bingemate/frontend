38f82e668d94e9e701eec0001065d9186d845841
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-02-27 17:08:01 +0100

    prepare routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {
   subscriptionLinks,
 } from './app-routing.module';
 import { accountLinks } from './pages/auth/auth-routing.module';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { accountLinks } from './pages/auth/auth-routing.module';
 })
 export class AppComponent {
   isCollapsed = false;
+  royuteName = '';
 
   accountLinks = Object.values(accountLinks).map(link => {
     return { ...link, path: `${navigationRoot.auth.path}/${link.path}` };
