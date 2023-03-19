970e24cf9788c61a6bff90c5aa2a8814e5465642
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-03-19 18:33:36 +0100

    add video player module

---
+++ src/app/feature/video-player/video-player.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+
+@NgModule({
+  declarations: [VideoPlayerComponent],
+  imports: [CommonModule],
+})
+export class VideoPlayerModule {}
