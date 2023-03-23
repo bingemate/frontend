7a81f2f5c858fe61d380b1610f02cf6800d1cc2b
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-03-23 12:40:14 +0100

    add account info retrieve on account page

--- src/app/pages/auth/auth-my-account/auth-my-account.component.ts
+++ src/app/pages/auth/auth-my-account/auth-my-account.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { AuthState } from '../../../core/auth/store/auth.state';
+import { Observable } from 'rxjs';
+import { UserModel } from '../../../shared/models/user.models';
 
 @Component({
   selector: 'app-auth-my-account',
   templateUrl: './auth-my-account.component.html',
   styleUrls: ['./auth-my-account.component.less'],
 })
-export class AuthMyAccountComponent {}
+export class AuthMyAccountComponent implements OnInit {
+  @Select(AuthState.user)
+  user$!: Observable<UserModel>;
+  user: UserModel | null = null;
+
+  ngOnInit() {
+    this.subscribeForAuthEvents();
+  }
+
+  subscribeForAuthEvents() {
+    this.user$.subscribe(user => {
+      this.user = user;
+    });
+  }
+}
