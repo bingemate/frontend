a609a61956d97b5408d2d90e4c4abdfedc85285a
pooetitu <pooetitu@gmail.com>
2023-04-26 21:37:51 +0200

    [BM-111]: Add current user id in headers

--- src/app/core/http-interceptors/auth-http.interceptor.ts
+++ src/app/core/http-interceptors/auth-http.interceptor.ts
@@ -6,18 +6,23 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Store } from '@ngxs/store';
+import { AuthState } from "../auth/store/auth.state";
 
 /** Passes user id in headers in local */
 @Injectable()
 export class LocalHttpAuthInterceptor implements HttpInterceptor {
+  constructor(private store: Store) {}
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    const authReq = request.clone({
-      headers: request.headers.set('user-id', 'id'),
-    });
-
+    const userId = this.store.selectSnapshot(AuthState.user)?.id;
+    let headers = request.headers;
+    if (userId) {
+      headers = headers.set('user-id', userId);
+    }
+    const authReq = request.clone({ headers });
     return next.handle(authReq);
   }
 }
