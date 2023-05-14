2f29819d9a3ed6e3cdae1e14796a378c45683289
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-14 14:42:04 +0200

    gitflow-feature-stash: BM-248-frontend-media feat: add MediaInfoModule and MediaInfoService
    This commit adds a new module, MediaInfoModule, and a new service, MediaInfoService. The module imports CommonModule and provides the MediaInfoService. The service is injected with HttpClient and provided in the root.

---
+++ src/app/feature/media-info/media-info.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MediaInfoService } from './media-info.service';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  providers: [MediaInfoService],
+})
+export class MediaInfoModule {}
