b170790e96c1cca62aad3434601c6433e8fbc4e1
Linter de ses morts <github-actions[bot]@users.noreply.github.com>
2023-05-18 21:33:59 +0000

    Linting fixes

--- src/app/feature/media-info/components/tv-info/tv-info.component.ts
+++ src/app/feature/media-info/components/tv-info/tv-info.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tv-info',
   templateUrl: './tv-info.component.html',
-  styleUrls: ['./tv-info.component.less']
+  styleUrls: ['./tv-info.component.less'],
 })
-export class TvInfoComponent {
-
-}
+export class TvInfoComponent {}
