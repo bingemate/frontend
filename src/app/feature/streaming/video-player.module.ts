b0ddb44a9c913f55fc3b2a2947d42f201e37508a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-19 20:58:59 +0200

    feat: Add NgZorroAntd and Videogular modules to video player
    This commit adds NgZorroAntd and Videogular modules to the video player. It also updates the HTML template of the video player component to include a card, overlay play, buffering, scrub bar, controls, track selector, quality selector, mute button, volume control and fullscreen button. Additionally it initializes audio options and subtitle list in the TypeScript file of the video player component.

--- src/app/feature/streaming/video-player.module.ts
+++ src/app/feature/streaming/video-player.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { VgCoreModule } from '@videogular/ngx-videogular/core';
 import { VgControlsModule } from '@videogular/ngx-videogular/controls';
 import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
 import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 import { VgStreamingModule } from '@videogular/ngx-videogular/streaming';
 import { VideoPlayerComponent } from './video-player/video-player.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { VgModuloModule } from '@videogular/ngx-videogular/modulo';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @NgModule({
   declarations: [VideoPlayerComponent],
@@ -16,6 +20,14 @@ import { VideoPlayerComponent } from './video-player/video-player.component';
     VgOverlayPlayModule,
     VgBufferingModule,
     VgStreamingModule,
+    NzCardModule,
+    NgIf,
+    VgBufferingModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgModuloModule,
+    NzIconModule,
+    NzSelectModule,
   ],
   exports: [VideoPlayerComponent],
 })
--- src/app/feature/streaming/video-player/video-player.component.ts
+++ src/app/feature/streaming/video-player/video-player.component.ts
@@ -1,8 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MediaFile } from '../../../shared/models/mdeia-file.model';
+import { BitrateOptions } from '@videogular/ngx-videogular/core';
+import { API_RESOURCE_URI } from '../../../shared/api-resource-uri/api-resources-uri';
 
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.less'],
 })
-export class VideoPlayerComponent {}
+export class VideoPlayerComponent implements OnInit {
+  @Input() mediaId = 'undefined';
+  @Input() mediaName = 'undefined';
+  @Input() mediaFile: MediaFile | undefined;
+
+  audioOptions: BitrateOptions[] = [];
+  audioList: string[] = [];
+
+  subtitleList: { label: string; url: string }[] = [];
+
+  videoUrl = '';
+  currentAudio = '';
+
+  ngOnInit() {
+    console.log(this.mediaId);
+    console.log(this.mediaName);
+    console.log(this.mediaFile);
+
+    if (this.mediaFile) {
+      this.videoUrl = `${API_RESOURCE_URI.STREAMING}/${this.mediaId}/${this.mediaFile.filename}`;
+      this.audioOptions = this.mediaFile.audioTracks.map(
+        (audioTrack, index) => {
+          return {
+            qualityIndex: index,
+            width: 0,
+            height: 0,
+            bitrate: 0,
+            mediaType: 'audio',
+            label: audioTrack.language,
+          };
+        }
+      );
+      this.audioList = this.mediaFile.audioTracks.map(
+        audioTrack =>
+          `${API_RESOURCE_URI.STREAMING}/${this.mediaId}/${audioTrack.filename}`
+      );
+      this.subtitleList = this.mediaFile.subtitleTracks.map(subtitleTrack => {
+        return {
+          label: subtitleTrack.language,
+          url: `${API_RESOURCE_URI.STREAMING}/${this.mediaId}/${subtitleTrack.filename}`,
+        };
+      });
+      this.currentAudio = this.audioList[0];
+    }
+  }
+
+  onSelectedAudio(event: BitrateOptions) {
+    this.currentAudio = this.audioList[event.qualityIndex];
+  }
+}
