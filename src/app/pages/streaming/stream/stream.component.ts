8a23e10c53ccd682702e69a1035567071763a2f3
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-19 20:59:14 +0200

    feat: Add video player to stream page
    This commit adds a video player component to the stream page. The video player is now wrapped in an nz-row and nz-col for better layout. The mediaId, mediaName, and mediaFile properties are also added to the StreamComponent class. Additionally, the NzGridModule is imported into StreamingModule for use with the new layout components.

--- src/app/pages/streaming/stream/stream.component.ts
+++ src/app/pages/streaming/stream/stream.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { MediaFile } from '../../../shared/models/mdeia-file.model';
 
 @Component({
   selector: 'app-stream',
   templateUrl: './stream.component.html',
   styleUrls: ['./stream.component.less'],
 })
-export class StreamComponent {}
+export class StreamComponent {
+  mediaId = '123456';
+  mediaName = "La croix de Jesus (Oui, j'avais pas d'idées ...)";
+  mediaFile: MediaFile = {
+    filename: 'index.m3u8',
+    duration: 1200,
+    audioTracks: [
+      {
+        filename: 'audio_1.m3u8',
+        language: 'JPN',
+      },
+    ],
+    subtitleTracks: [
+      {
+        filename: 'subtitle_2.vtt',
+        language: 'FRA',
+      },
+    ],
+  };
+}
--- src/app/pages/streaming/streaming.module.ts
+++ src/app/pages/streaming/streaming.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { StreamingRoutingModule } from './streaming-routing.module';
 import { StreamComponent } from './stream/stream.component';
 import { VideoPlayerModule } from '../../feature/streaming/video-player.module';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
   declarations: [StreamComponent],
-  imports: [CommonModule, StreamingRoutingModule, VideoPlayerModule],
+  imports: [
+    CommonModule,
+    StreamingRoutingModule,
+    VideoPlayerModule,
+    NzGridModule,
+  ],
 })
 export class StreamingModule {}
