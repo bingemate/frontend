641e579723ca3d2aa2203dc64fcd98df7132ff2f
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-20 15:51:55 +0200

    feat: Add MEDIA_INDEXER API Resource URI
    This commit adds a new API resource URI for the Media Indexer. The URI is added to the existing list of URIs in the `API_RESOURCE_URI` object.

--- src/app/shared/api-resource-uri/api-resources-uri.ts
+++ src/app/shared/api-resource-uri/api-resources-uri.ts
@@ -2,6 +2,8 @@ import { environment as env } from '../../../environments/environment';
 
 export const API_RESOURCE_URI = {
   HTTP_BIN: env.apiUrl + '/httpbin',
+  // Media Indexer
+  MEDIA_INDEXER: `${env.apiUrl}/media-indexer`,
   // Users
   USERS: `${env.apiUrl}/users`,
   USERS_SEARCH: `${env.apiUrl}/users/search`,
