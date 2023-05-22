0e64ca5f59bfd6a0b2189208c9c3dc013ff3a4f3
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-22 20:40:41 +0200

    feat: Add TV show rating and present icon
    This commit adds a TV show rating feature to the TV info card component. It also adds a present icon for currently airing shows. The component now uses routerLink to navigate to the TV show view page.

--- src/app/feature/media-info/components/tv-info-card/tv-info-card.component.ts
+++ src/app/feature/media-info/components/tv-info-card/tv-info-card.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TvShowResponse } from '../../../../shared/models/media.models';
+import { tvShowViewPath } from '../../../../pages/medias/medias-routing.module';
 
 @Component({
   selector: 'app-tv-info-card',
   templateUrl: './tv-info-card.component.html',
   styleUrls: ['./tv-info-card.component.less'],
 })
-export class TvInfoCardComponent {
+export class TvInfoCardComponent implements OnInit {
+  tvShowViewPath = tvShowViewPath;
   @Input() tv?: TvShowResponse;
+
+  ngOnInit(): void {
+    this.tvShowViewPath = tvShowViewPath + '/' + this.tv?.id;
+  }
+
+  getRate(): number {
+    return Math.round(this.tv?.voteAverage ?? 0) / 2;
+  }
 }
