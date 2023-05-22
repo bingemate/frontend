ee60de61d94b439a12623d0cfe5be7ef78158fe8
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-22 20:41:45 +0200

    feat(tv-info): Add TV show details and actors list
    This commit adds a new component, TvInfoComponent, which displays detailed information about a TV show. It also includes an actors list with pagination. The component uses several Angular Material components to display the data in a structured way.

--- src/app/feature/media-info/components/tv-info/tv-info.component.ts
+++ src/app/feature/media-info/components/tv-info/tv-info.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { navigationRoot } from '../../../../app-routing.module';
+import { streamingLinks } from '../../../../pages/streaming/streaming-routing.module';
+import { mediasLinks } from '../../../../pages/medias/medias-routing.module';
+import { Person, TvShowResponse } from '../../../../shared/models/media.models';
 
 @Component({
   selector: 'app-tv-info',
   templateUrl: './tv-info.component.html',
   styleUrls: ['./tv-info.component.less'],
 })
-export class TvInfoComponent {}
+export class TvInfoComponent {
+  readonly streamPath = `/${navigationRoot.streaming.path}/${streamingLinks.stream.path}/`;
+  readonly tvsByGenrePath = `/${navigationRoot.medias.path}/${mediasLinks.tv_shows_by_genre.path}/`;
+  readonly tvsByNetworkPath = `/${navigationRoot.medias.path}/${mediasLinks.tv_shows_by_network.path}/`;
+
+  @Input() tv: TvShowResponse | undefined;
+  actorsCurrentPage = 1;
+  actorsPageSize = 5;
+
+  getRate(): number {
+    return Math.round(this.tv?.voteAverage ?? 0) / 2;
+  }
+
+  getActorsSlice(): Person[] {
+    const start = (this.actorsCurrentPage - 1) * this.actorsPageSize;
+    const end = start + this.actorsPageSize;
+    return this.tv?.actors.slice(start, end) ?? [];
+  }
+
+  onActorsPageChange(page: number): void {
+    this.actorsCurrentPage = page;
+  }
+}
--- src/app/feature/media-info/media-info.module.ts
+++ src/app/feature/media-info/media-info.module.ts
@@ -69,6 +69,7 @@ import { ActorInfoComponent } from './components/actor-info/actor-info.component
     TvListComponent,
     MovieInfoComponent,
     ActorInfoComponent,
+    TvInfoComponent,
   ],
 })
 export class MediaInfoModule {}
