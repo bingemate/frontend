915595ff6d53bed3910e6c94288fde97c6be3ca4
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-23 21:18:47 +0200

    feat: Add movie recommendations to movie view
    This commit adds a new section to the movie view page that displays recommended movies. The `MovieViewComponent` now uses the `MediaDiscoverService` to fetch recommended movies and display them in a list below the main movie information.

--- src/app/pages/medias/movie-view/movie-view.component.ts
+++ src/app/pages/medias/movie-view/movie-view.component.ts
@@ -2,28 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieResponse } from '../../../shared/models/media.models';
 import { MediaInfoService } from '../../../feature/media-info/media-info.service';
+import { MediaDiscoverService } from '../../../feature/media-info/media-discover.service';
 
 @Component({
   selector: 'app-movie-view',
   templateUrl: './movie-view.component.html',
   styleUrls: ['./movie-view.component.less'],
 })
-export class MovieViewComponent implements OnInit {
+export class MovieViewComponent {
   movieId?: number;
   movie?: MovieResponse;
+  movieRecommendations: MovieResponse[] = [];
 
   constructor(
     currentRoute: ActivatedRoute,
-    private mediaInfoService: MediaInfoService
+    private mediaInfoService: MediaInfoService,
+    private mediaDiscoverService: MediaDiscoverService
   ) {
     currentRoute.params.subscribe(params => {
       this.movieId = params['id'];
+      this.onGetMovie();
     });
   }
-
-  ngOnInit(): void {
+  onGetMovie() {
     this.mediaInfoService.getMovieInfo(this.movieId ?? 0).subscribe(movie => {
       this.movie = movie;
     });
+    this.mediaDiscoverService
+      .getMovieRecommendations(this.movieId ?? 0)
+      .subscribe(movieRecommendations => {
+        this.movieRecommendations = movieRecommendations;
+      });
   }
 }
