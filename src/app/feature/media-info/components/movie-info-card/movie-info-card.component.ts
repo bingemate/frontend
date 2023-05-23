7823e06863d4c04aabe630e80de33c05952dd8d6
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-23 21:17:49 +0200

    feat(movie-info-card): add click event to view movie
    This commit adds a click event to the movie title in the MovieInfoCardComponent that navigates to the movie view page. The ngOnInit method was removed and replaced with a constructor that injects Router.

--- src/app/feature/media-info/components/movie-info-card/movie-info-card.component.ts
+++ src/app/feature/media-info/components/movie-info-card/movie-info-card.component.ts
@@ -1,21 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { movieViewPath } from 'src/app/pages/medias/medias-routing.module';
 import { MovieResponse } from '../../../../shared/models/media.models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-info-card',
   templateUrl: './movie-info-card.component.html',
   styleUrls: ['./movie-info-card.component.less'],
 })
-export class MovieInfoCardComponent implements OnInit {
-  movieViewPath = movieViewPath;
+export class MovieInfoCardComponent {
   @Input() movie?: MovieResponse;
 
-  ngOnInit(): void {
-    this.movieViewPath = movieViewPath + '/' + this.movie?.id;
-  }
+  constructor(private router: Router) {}
 
   getRate(): number {
     return Math.round(this.movie?.voteAverage ?? 0) / 2;
+  }
+
+  onViewMovie(): void {
+    this.router
+      .navigate([movieViewPath, this.movie?.id], {
+        onSameUrlNavigation: 'reload',
+      })
+      .then();
   }
 }
