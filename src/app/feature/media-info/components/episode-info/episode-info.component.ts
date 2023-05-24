b7a8e4607fd56817dd94d389a6b466b8a84a75c8
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-24 20:31:28 +0200

    feat: Remove episode-info and episode-info-card components
    This commit removes the episode-info and episode-info-card components from the media-info feature. The corresponding HTML, LESS, and TypeScript files have been deleted as well.

--- src/app/feature/media-info/components/episode-info/episode-info.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-episode-info',
-  templateUrl: './episode-info.component.html',
-  styleUrls: ['./episode-info.component.less'],
-})
-export class EpisodeInfoComponent {}
