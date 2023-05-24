edda568e1a8dd2e0befc11d58341cbf0d92aebc0
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-24 20:32:41 +0200

    feat(tv-info): Add episode list by season
    This commit adds a new feature to the TV Info component that displays a list of episodes for each season in a tabbed interface. The `getSeasons()` method generates an array of numbers representing the seasons, and the `app-episode-info-list` component is used to display the episode information. Additionally, some unused imports were removed from the file.

--- src/app/feature/media-info/components/tv-info/tv-info.component.ts
+++ src/app/feature/media-info/components/tv-info/tv-info.component.ts
@@ -2,7 +2,13 @@ import { Component, Input } from '@angular/core';
 import { navigationRoot } from '../../../../app-routing.module';
 import { streamingLinks } from '../../../../pages/streaming/streaming-routing.module';
 import { mediasLinks } from '../../../../pages/medias/medias-routing.module';
-import { Person, TvShowResponse } from '../../../../shared/models/media.models';
+import {
+  Person,
+  TvEpisodeResponse,
+  TvShowResponse,
+} from '../../../../shared/models/media.models';
+import { MediaInfoService } from '../../media-info.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tv-info',
@@ -10,7 +16,6 @@ import { Person, TvShowResponse } from '../../../../shared/models/media.models';
   styleUrls: ['./tv-info.component.less'],
 })
 export class TvInfoComponent {
-  readonly streamPath = `/${navigationRoot.streaming.path}/${streamingLinks.stream.path}/`;
   readonly tvsByGenrePath = `/${navigationRoot.medias.path}/${mediasLinks.tv_shows_by_genre.path}/`;
   readonly tvsByActorPath = `/${navigationRoot.medias.path}/${mediasLinks.tv_show_by_actor.path}/`;
   readonly tvsByNetworkPath = `/${navigationRoot.medias.path}/${mediasLinks.tv_shows_by_network.path}/`;
@@ -18,6 +23,10 @@ export class TvInfoComponent {
   @Input() tv: TvShowResponse | undefined;
   actorsCurrentPage = 1;
   actorsPageSize = 5;
+
+  getSeasons(): number[] {
+    return Array.from({ length: this.tv?.seasonsCount ?? 0 }, (_, i) => i + 1);
+  }
 
   getRate(): number {
     return Math.round(this.tv?.voteAverage ?? 0) / 2;
@@ -32,4 +41,6 @@ export class TvInfoComponent {
   onActorsPageChange(page: number): void {
     this.actorsCurrentPage = page;
   }
+
+  protected readonly Array = Array;
 }
