89d5d645b152cda92e54d39aa3b0492c606005e4
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-30 18:54:54 +0200

    feat(comment): add comment service
    This commit adds a new file `comment.service.ts` which contains the implementation of the CommentService class. The CommentService class provides methods to get media and user comments, create, update and delete comments.

---
+++ src/app/feature/comment/comment.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_RESOURCE_URI } from '../../shared/api-resource-uri/api-resources-uri';
+import {
+  CommentRequest,
+  CommentResponse,
+  CommentResults,
+} from '../../shared/models/comment.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  constructor(private readonly http: HttpClient) {}
+
+  getMediaComments(mediaId: number, page = 1): Observable<CommentResults> {
+    const params = new HttpParams().set('page', page);
+    return this.http.get<CommentResults>(
+      `${API_RESOURCE_URI.MEDIA_INFO}/comment/media/${mediaId}`,
+      { params }
+    );
+  }
+
+  getUserComments(userId: string, page = 1): Observable<CommentResults> {
+    const params = new HttpParams().set('page', page);
+    return this.http.get<CommentResults>(
+      `${API_RESOURCE_URI.MEDIA_INFO}/comment/user/${userId}`,
+      { params }
+    );
+  }
+
+  createComment(mediaId: number, content: string): Observable<CommentResponse> {
+    const requestContent: CommentRequest = {
+      content,
+    };
+    return this.http.post<CommentResponse>(
+      `${API_RESOURCE_URI.MEDIA_INFO}/comment/media/${mediaId}`,
+      requestContent
+    );
+  }
+
+  updateComment(
+    commentId: number,
+    content: string
+  ): Observable<CommentResponse> {
+    const requestContent: CommentRequest = {
+      content,
+    };
+    return this.http.put<CommentResponse>(
+      `${API_RESOURCE_URI.MEDIA_INFO}/comment/${commentId}`,
+      requestContent
+    );
+  }
+
+  deleteComment(commentId: number): Observable<string> {
+    return this.http.delete<string>(
+      `${API_RESOURCE_URI.MEDIA_INFO}/comment/${commentId}`
+    );
+  }
+}
