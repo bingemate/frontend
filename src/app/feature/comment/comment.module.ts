485e695a912168759b23ffbe6ef983b17102d13a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-31 15:20:14 +0200

    feat(comment): add new comment component
    This commit adds a new comment component to the comment module. The new component includes a form for adding comments and emits an event when a new comment is added. The necessary dependencies are also imported into the module.

--- src/app/feature/comment/comment.module.ts
+++ src/app/feature/comment/comment.module.ts
@@ -1,12 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CommentListComponent } from './components/comment-list/comment-list.component';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+} from '@angular/cdk/scrolling';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzCommentModule } from 'ng-zorro-antd/comment';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NewCommentComponent } from './components/new-comment/new-comment.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [CommentListComponent, NewCommentComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NzListModule,
+    NzButtonModule,
+    NzIconModule,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualScrollViewport,
+    CdkVirtualForOf,
+    NzPopconfirmModule,
+    NzCommentModule,
+    NzSpaceModule,
+    NzTypographyModule,
+    NzFormModule,
+    NzInputModule,
+    ReactiveFormsModule,
+  ],
+  exports: [CommentListComponent, NewCommentComponent],
 })
-export class CommentModule { }
+export class CommentModule {}
---
+++ src/app/feature/comment/components/new-comment/new-comment.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommentResponse } from '../../../../shared/models/comment.models';
+import { CommentService } from '../../comment.service';
+import { NotificationsService } from '../../../../core/notifications/notifications.service';
+
+@Component({
+  selector: 'app-new-comment',
+  templateUrl: './new-comment.component.html',
+  styleUrls: ['./new-comment.component.less'],
+})
+export class NewCommentComponent {
+  @Input() mediaId = 0;
+  @Output() newComment: EventEmitter<CommentResponse> = new EventEmitter();
+
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private commentService: CommentService,
+    private notificationsService: NotificationsService
+  ) {
+    this.form = this.formBuilder.group({
+      comment: [null, [Validators.maxLength(1000)]],
+    });
+  }
+
+  onSubmit(): void {
+    this.commentService
+      .createComment(this.mediaId, this.form.value.comment)
+      .subscribe(comment => {
+        this.notificationsService.success('Commentaire ajouté');
+        this.newComment.emit(comment);
+        this.form.reset();
+      });
+  }
+}
