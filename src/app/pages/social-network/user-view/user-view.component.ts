52fd7a8e0edfdf52489ed6cd8bdcb7e5dc7a7b27
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-01 19:38:26 +0200

    fix

--- src/app/pages/social-network/user-view/user-view.component.ts
+++ src/app/pages/social-network/user-view/user-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserResponse } from '../../../shared/models/user.models';
 import {
   CommentResults,
