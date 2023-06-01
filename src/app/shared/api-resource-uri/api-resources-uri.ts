5e882c0cb42345115b4631130339132432710c4b
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-01 19:23:37 +0200

    feat: Update version to 0.17.1 and API resource URI
    The package.json and package-lock.json files have been updated to version 0.17.1, while the API_RESOURCE_URI in api-resources-uri.ts has been modified to use env.apiUrl for KEYCLOAK_SERVICE instead of a hardcoded URL.

--- src/app/shared/api-resource-uri/api-resources-uri.ts
+++ src/app/shared/api-resource-uri/api-resources-uri.ts
@@ -11,6 +11,6 @@ export const API_RESOURCE_URI = {
   // Users
   USERS: `${env.apiUrl}/users`,
   USERS_SEARCH: `${env.apiUrl}/users/search`,
-  // KEYCLOAK_SERVICE: `${env.apiUrl}/keycloak-service`,
-  KEYCLOAK_SERVICE: `http://localhost:8081/keycloak-service`,
+  KEYCLOAK_SERVICE: `${env.apiUrl}/keycloak-service`,
+  // KEYCLOAK_SERVICE: `http://localhost:8081/keycloak-service`,
 };
