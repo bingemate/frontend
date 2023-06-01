691bb7cadf5a0e2c694a85da72d07fe10308f45f
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-01 18:17:13 +0200

    feat: Add user search and list components
    This commit adds the UserSearchComponent and UserListComponent to the UserModule. It also imports necessary modules such as NgxsModule, NzButtonModule, NzCardModule, NzGridModule, NzIconModule, NzInputModule, ReactiveFormsModule, FormsModule and others. Finally it exports the UserSearchComponent for use in other modules.

--- src/app/feature/user/user.module.ts
+++ src/app/feature/user/user.module.ts
@@ -1,13 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { UserSearchComponent } from './components/user-search/user-search.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzWaveModule } from 'ng-zorro-antd/core/wave';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserSearchState } from './store/user-search.state';
+import { NgxsModule } from '@ngxs/store';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { UserListComponent } from './components/user-list/user-list.component';
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+} from '@angular/cdk/scrolling';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [UserSearchComponent, UserListComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NzButtonModule,
+    NzCardModule,
+    NzGridModule,
+    NzIconModule,
+    NzInputModule,
+    NzProgressModule,
+    NzWaveModule,
+    ReactiveFormsModule,
+    FormsModule,
     NgxsModule.forFeature([UserSearchState]),
+    NzEmptyModule,
+    NzListModule,
+    CdkVirtualForOf,
+    CdkVirtualScrollViewport,
+    CdkFixedSizeVirtualScroll,
+    RouterLink,
+  ],
+  exports: [UserSearchComponent],
 })
-export class UserModule { }
+export class UserModule {}
