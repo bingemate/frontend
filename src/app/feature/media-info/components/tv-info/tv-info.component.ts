27dce995ff3dcb0cea9fc8d8ade5ecab805b7806
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-03 11:37:53 +0200

    feat(media-info): Add watchlist functionality to TV show component
    This commit adds the ability for users to add TV shows to their watchlist, change the status of a show on their watchlist, and remove a show from their watchlist. The changes include modifications to the HTML and TypeScript files of the TV info component, as well as importing necessary modules and services. The commit follows conventional commit methodology.

--- src/app/feature/media-info/components/tv-info/tv-info.component.ts
+++ src/app/feature/media-info/components/tv-info/tv-info.component.ts
@@ -3,11 +3,16 @@ import { navigationRoot } from '../../../../app-routing.module';
 import { mediasLinks } from '../../../../pages/medias/medias-routing.module';
 import { Person, TvShowResponse } from '../../../../shared/models/media.models';
 import {
+  WatchlistItem,
   WatchListStatus,
   WatchListType,
 } from '../../../../shared/models/watchlist.models';
 import { WatchlistService } from '../../../watchlist/watchlist.service';
 import { NotificationsService } from '../../../../core/notifications/notifications.service';
+import { Select } from '@ngxs/store';
+import { AuthState } from '../../../../core/auth/store/auth.state';
+import { Observable } from 'rxjs';
+import { UserResponse } from '../../../../shared/models/user.models';
 
 @Component({
   selector: 'app-tv-info',
@@ -15,25 +20,31 @@ import { NotificationsService } from '../../../../core/notifications/notificatio
   styleUrls: ['./tv-info.component.less'],
 })
 export class TvInfoComponent implements OnChanges {
+  @Select(AuthState.user)
+  readonly user$!: Observable<UserResponse>;
+  userId = '';
+
   readonly tvsByGenrePath = `/${navigationRoot.medias.path}/${mediasLinks.tv_shows_by_genre.path}/`;
   readonly tvsByActorPath = `/${navigationRoot.medias.path}/${mediasLinks.tv_show_by_actor.path}/`;
   readonly tvsByNetworkPath = `/${navigationRoot.medias.path}/${mediasLinks.tv_shows_by_network.path}/`;
   readonly statusNames = Object.values(WatchListStatus);
-  readonly statusMap = {
-    WATCHING: 'En cours',
-    PLAN_TO_WATCH: 'Prévu',
-    FINISHED: 'Terminé',
-    ABANDONED: 'Abandonné',
-  };
 
   @Input() tv: TvShowResponse | undefined;
   actorsCurrentPage = 1;
   actorsPageSize = 5;
   isMediaInWatchList = true;
+  watchlistItem: WatchlistItem | undefined;
+
   constructor(
     private readonly notificationsService: NotificationsService,
     private watchlistService: WatchlistService
-  ) {}
+  ) {
+    this.user$.subscribe(user => {
+      if (user) {
+        this.userId = user.id;
+      }
+    });
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (
@@ -44,6 +55,9 @@ export class TvInfoComponent implements OnChanges {
       this.watchlistService.getWatchlistItem(this.tv.id).subscribe(item => {
         if (!item) {
           this.isMediaInWatchList = false;
+        } else {
+          this.isMediaInWatchList = true;
+          this.watchlistItem = item;
         }
       });
     }
@@ -79,10 +93,37 @@ export class TvInfoComponent implements OnChanges {
         })
         .subscribe(() => {
           this.notificationsService.success(
-            'La série a été ajouté aux films suivis'
+            'La série a été ajouté aux séries suivis'
           );
           this.isMediaInWatchList = true;
+          this.watchlistItem = {
+            mediaId: this.tv!.id,
+            mediaType: WatchListType.SHOW,
+            status,
+            userId: this.userId,
+          };
         });
     }
+  }
+
+  changeShowStatus(status: WatchListStatus) {
+    this.watchlistItem!.status = status;
+    this.watchlistService
+      .updateWatchlistItem(this.watchlistItem!)
+      .subscribe(() =>
+        this.notificationsService.success('Liste de suivie modifié')
+      );
+  }
+
+  removeShowWatchlist() {
+    this.watchlistService
+      .removeFromWatchlist(this.watchlistItem!.mediaId)
+      .subscribe(() => {
+        this.isMediaInWatchList = false;
+        this.watchlistItem = undefined;
+        this.notificationsService.success(
+          'La série a été retiré des séries suivis'
+        );
+      });
   }
 }
