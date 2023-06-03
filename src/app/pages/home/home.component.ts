201579b1013fd4e1adf0ba750af879804b11cd1a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-03 11:57:51 +0200

    refactor: Rename home.module.ts to homepage.module.ts
    The commit renames the `home.module.ts` file to `homepage.module.ts`. It also updates the import statements in the `app-routing.module.ts`, and adds two new components, `app-noauth-home` and `app-auth-home`, to the `home.component.html` file. Finally, it modifies the `HomeComponent` class by adding a new property called `isAuth`, which is set based on whether or not a user is authenticated.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { AuthState } from '../../core/auth/store/auth.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  @Select(AuthState.isAuthenticated)
+  isAuthenticated$!: Observable<boolean>;
+  isAuth = false;
+
+  constructor() {
+    this.isAuthenticated$.subscribe(isAuth => {
+      this.isAuth = isAuth;
+    });
+  }
+}
--- src/app/pages/home/homepage.module.ts
+++ src/app/pages/home/homepage.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NgIf } from '@angular/common';
+import { HomeModule } from '../../feature/home/home.module';
 
 @NgModule({
-  imports: [HomeRoutingModule],
+  imports: [HomeRoutingModule, NgIf, HomeModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
 })
-export class HomeModule {}
+export class HomepageModule {}
