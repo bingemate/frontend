74ef31b2c7b19dc99d31d1f1764d70b593df2b59
pooetitu <pooetitu@gmail.com>
2023-06-09 20:52:57 +0200

    [BM-221]: Rework genre display and fix data not displayed on load

--- src/app/shared/components/stats-daily-viewed-genre/stats-daily-viewed-genre.component.ts
+++ src/app/shared/components/stats-daily-viewed-genre/stats-daily-viewed-genre.component.ts
@@ -1,17 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { ChartConfiguration, ChartType } from 'chart.js';
+import { StatDisplay, Statistic } from '../../models/statistic';
 import { getDateDays } from '../../utils/date.utils';
-import { Statistic } from '../../models/statistic.models';
+import { MediaInfoService } from '../../../feature/media-info/media-info.service';
+import { catchError, forkJoin, map } from 'rxjs';
+import { Genre } from '../../models/media.models';
 
 @Component({
   selector: 'app-stats-daily-viewed-genre',
   templateUrl: './stats-daily-viewed-genre.component.html',
   styleUrls: ['./stats-daily-viewed-genre.component.less'],
 })
-export class StatsDailyViewedGenreComponent implements OnInit {
+export class StatsDailyViewedGenreComponent implements OnInit, OnChanges {
   @Input()
   stats: Statistic[] = [];
-  readonly lineChartType: ChartType = 'pie';
+
+  sevenDays: StatDisplay = { data: [], labels: [] };
+  oneMonth: StatDisplay = { data: [], labels: [] };
+  sixMonth: StatDisplay = { data: [], labels: [] };
+  readonly lineChartType: ChartType = 'radar';
   readonly lineChartOptions: ChartConfiguration['options'] = {
     plugins: {
       legend: {
@@ -25,60 +38,105 @@ export class StatsDailyViewedGenreComponent implements OnInit {
     datasets: [],
   };
 
+  constructor(private mediaService: MediaInfoService) {}
+
   ngOnInit(): void {
-    this.setDailyViewSevenDaysPeriod();
+    this.updateData();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['stats'].currentValue !== changes['stats'].previousValue) {
+      this.updateData();
+    }
   }
 
   setDailyViewSevenDaysPeriod() {
     this.selectedPeriod = '7 jours';
-    this.setChartData(7);
+    this.updateChartData(this.sevenDays);
   }
 
   setDailyViewMonthPeriod() {
     this.selectedPeriod = '1 mois';
-    this.setChartData(30);
+    this.updateChartData(this.oneMonth);
   }
 
   setDailyViewSemesterPeriod() {
     this.selectedPeriod = '6 mois';
-    this.setChartData(180);
+    this.updateChartData(this.sixMonth);
   }
 
-  private updateChartData(data: number[], labels: string[]): void {
+  private updateChartData(stats: StatDisplay): void {
     this.lineChartData = {
       datasets: [
         {
-          data,
+          data: stats.data,
         },
       ],
-      labels,
+      labels: stats.labels,
     };
   }
 
-  private setChartData(period: number) {
-    let stats = this.stats.filter(
-      stat =>
-        getDateDays(new Date().getTime()) -
-          getDateDays(stat.startedAt.getTime()) <=
-        period
-    );
-    stats = stats.sort((a, b) => a.startedAt.getTime() - b.startedAt.getTime());
-    const watchTimePerDay = this.getWatchTimePerDay(stats);
-    const labels = Array.from(watchTimePerDay.keys());
-    const data = Array.from(watchTimePerDay.values());
-    this.updateChartData(data, labels);
+  private updateData() {
+    forkJoin(
+      this.stats.map(stat =>
+        this.mediaService.getTvShowShortInfo(stat.mediaId).pipe(
+          map(media => ({
+            stat,
+            media,
+          })),
+          catchError(() => {
+            return this.mediaService.getMovieShortInfo(stat.mediaId).pipe(
+              map(media => ({
+                stat,
+                media,
+              }))
+            );
+          })
+        )
+      )
+    ).subscribe(stats => {
+      this.sevenDays = this.getPeriodData(stats, 7);
+      this.oneMonth = this.getPeriodData(stats, 30);
+      this.sixMonth = this.getPeriodData(stats, 180);
+      if (this.selectedPeriod === '7 jours') {
+        this.setDailyViewSevenDaysPeriod();
+      } else if (this.selectedPeriod === '1 mois') {
+        this.setDailyViewMonthPeriod();
+      } else {
+        this.setDailyViewSemesterPeriod();
+      }
+    });
   }
 
-  private getWatchTimePerDay(stats: Statistic[]) {
+  private getPeriodData(
+    stats: readonly { stat: Statistic; media: any }[],
+    period: number
+  ) {
+    let statsFiltered = stats.filter(
+      stat =>
+        getDateDays(new Date().getTime()) -
+          getDateDays(stat.stat.startedAt.getTime()) <=
+        period
+    );
+    statsFiltered = statsFiltered.sort(
+      (a, b) => a.stat.startedAt.getTime() - b.stat.startedAt.getTime()
+    );
+    const watchTimePerDay = this.getWatchTimePerDay(statsFiltered);
+    const labels = Array.from(watchTimePerDay.keys());
+    const data = Array.from(watchTimePerDay.values());
+    return { labels, data };
+  }
+
+  private getWatchTimePerDay(stats: { stat: Statistic; media: any }[]) {
     const data: Map<string, number> = new Map<string, number>();
     stats.forEach(stat => {
-      const key = 'Genre'; // TODO get media genre
-      const value = data.get(key);
-      if (!data.has(key) || !value) {
-        data.set(key, 1);
-      } else {
-        data.set(key, value + 1);
-      }
+      stat.media.genres.forEach((genre: Genre) => {
+        const value = data.get(genre.name);
+        if (!data.has(genre.name) || !value) {
+          data.set(genre.name, 1);
+        } else {
+          data.set(genre.name, value + 1);
+        }
+      });
     });
     return data;
   }
