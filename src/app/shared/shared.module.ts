da929c8bc231217f5ad64a11474cdab90367c52f
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-11 17:19:51 +0200

    refactor: Remove MediaNamePipe from SharedModule
    The MediaNamePipe was removed from the SharedModule as it is no longer being used. The getMediaInfo method was also removed from the MediaInfoService.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,6 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { BooleanYesNoPipe } from './pipes/boolean-yes-no.pipe';
 import { EmptyStringPipe } from './pipes/empty-string.pipe';
-import { MediaNamePipe } from './pipes/media-name.pipe';
 import { MediaLinkPipe } from './pipes/media-link.pipe';
 import { UsernamePipe } from './pipes/username.pipe';
 import { MovieNamePipe } from './pipes/movie-name.pipe';
@@ -18,7 +17,6 @@ import { MovieWatchlistStatusPipe } from './pipes/movie-watchlist-status.pipe';
     AlbumViewComponent,
     BooleanYesNoPipe,
     EmptyStringPipe,
-    MediaNamePipe,
     MediaLinkPipe,
     UsernamePipe,
     MovieNamePipe,
@@ -31,7 +29,6 @@ import { MovieWatchlistStatusPipe } from './pipes/movie-watchlist-status.pipe';
     AlbumViewComponent,
     BooleanYesNoPipe,
     EmptyStringPipe,
-    MediaNamePipe,
     MediaLinkPipe,
     UsernamePipe,
     MovieNamePipe,
