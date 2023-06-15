b17cf33c65d6cfaba38c7fb6604ce83245a8bddc
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-15 19:47:55 +0200

    feat(subscription): add actions to subscription details
    This commit adds new actions to the subscription details page, including the ability to view invoices, change payment methods, and cancel subscriptions. It also adjusts the layout of the page based on screen size using a BreakpointObserver.

--- src/app/pages/subscription/subscription/subscription.component.ts
+++ src/app/pages/subscription/subscription/subscription.component.ts
@@ -3,6 +3,7 @@ import { PaymentService } from '../../../feature/subscription/payment.service';
 import { SubscriptionModel } from '../../../shared/models/streaming.model';
 import { navigationRoot } from '../../../app-routing.module';
 import { subscriptionLinks } from '../subscriptions-routing.module';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-subscription',
@@ -10,11 +11,22 @@ import { subscriptionLinks } from '../subscriptions-routing.module';
   styleUrls: ['./subscription.component.less'],
 })
 export class SubscriptionComponent implements OnInit {
+  isOnPhone = false;
+
   readonly subscriptionBillingPath = `/${navigationRoot.subscriptions.path}/${subscriptionLinks.billing.path}/`;
   loading = false;
   subscription?: SubscriptionModel;
 
-  constructor(private paymentService: PaymentService) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private paymentService: PaymentService
+  ) {
+    this.breakpointObserver
+      .observe([Breakpoints.HandsetPortrait])
+      .subscribe(result => {
+        this.isOnPhone = result.matches;
+      });
+  }
 
   ngOnInit() {
     this.paymentService
