5e8271b69c6dd2db96d70927d8c7a3baea6040ad
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-20 22:18:42 +0200

    feat(home-page): add lifecycle hooks and subscription management
    This commit adds the OnInit and OnDestroy lifecycle hooks to HomePageComponent, as well as a subscriptions array to manage subscriptions. The isAuthenticated$ observable is now subscribed to in ngOnInit and unsubscribed from in ngOnDestroy.

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { AuthState } from '../../core/auth/store/auth.state';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit, OnDestroy {
   @Select(AuthState.isAuthenticated)
   isAuthenticated$!: Observable<boolean>;
   isAuth = false;
 
-  constructor() {
-    this.isAuthenticated$.subscribe(isAuth => {
-      this.isAuth = isAuth;
-    });
+  subscriptions: Subscription[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.isAuthenticated$.subscribe(isAuth => {
+        this.isAuth = isAuth;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 }
