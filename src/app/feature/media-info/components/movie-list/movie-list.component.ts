2f6471038c2e3a2c3ac1c8945951acd3b442df08
pooetitu <pooetitu@gmail.com>
2023-06-20 21:38:13 +0200

    [BM-416]: Fix image ratio

--- src/app/feature/media-info/components/movie-list/movie-list.component.ts
+++ src/app/feature/media-info/components/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MovieResponse } from '../../../../shared/models/media.models';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-movie-list',
@@ -8,4 +9,10 @@ import { MovieResponse } from '../../../../shared/models/media.models';
 })
 export class MovieListComponent {
   @Input() movies: MovieResponse[] = [];
+  isOnPhone = false;
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
+      this.isOnPhone = result.matches;
+    });
+  }
 }
