30405c5367345bc453737133f281b7b239d33c93
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-07-02 17:11:48 +0200

    feat(actor-search): Add actor search component
    This commit adds the actor search component, which allows users to search for actors/actresses. The component includes an input field with a search button, as well as options to filter adult content. It also displays the search results in a paginated list.
    
    - Added HTML template for the actor search component
    - Added styles for the actor search component
    - Added TypeScript file for the actor search component
    - Implemented functionality to handle user input and perform searches
    - Implemented pagination for displaying search results

---
+++ src/app/feature/media-search/components/actor-search/actor-search.component.ts
@@ -0,0 +1,107 @@
+import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { ActorSearchState } from '../../store/actor-search.state';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { ActorsResults } from '../../../../shared/models/media.models';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { debounceTime } from 'rxjs/operators';
+import { ActorSearchActions } from '../../store/actor-search.actions';
+
+@Component({
+  selector: 'app-actor-search',
+  templateUrl: './actor-search.component.html',
+  styleUrls: ['./actor-search.component.less'],
+})
+export class ActorSearchComponent {
+  isOnPhone = false;
+
+  @Select(ActorSearchState.query)
+  query$!: Observable<string>;
+  query = '';
+  @Select(ActorSearchState.loading)
+  searching$!: Observable<boolean>;
+  @Select(ActorSearchState.actors)
+  results$!: Observable<ActorsResults>;
+  @Select(ActorSearchState.currentPage)
+  currentPage$!: Observable<number>;
+  @Select(ActorSearchState.adult)
+  adult$!: Observable<boolean>;
+
+  adultCounter = 0;
+
+  inputSubject: Subject<string> = new Subject<string>();
+  private subscription: Subscription;
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private store: Store
+  ) {
+    this.subscription = this.inputSubject
+      .pipe(debounceTime(1000))
+      .subscribe(() => {
+        this.search();
+      });
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.breakpointObserver
+        .observe([Breakpoints.Handset])
+        .subscribe(result => {
+          this.isOnPhone = result.matches;
+        })
+    );
+
+    this.subscriptions.push(
+      this.query$.subscribe(query => {
+        this.query = query;
+      })
+    );
+  }
+
+  onInput() {
+    this.inputSubject.next(this.query);
+  }
+
+  manualSearch() {
+    this.subscription.unsubscribe();
+    this.search();
+    this.subscription = this.inputSubject
+      .pipe(debounceTime(1000))
+      .subscribe(() => {
+        this.search();
+      });
+  }
+
+  search() {
+    if (this.query.length > 0) {
+      this.store.dispatch(
+        new ActorSearchActions.Search({
+          query: this.query,
+          page: 1,
+        })
+      );
+    }
+  }
+
+  click() {
+    this.adultCounter++;
+    if (this.adultCounter === 6) {
+      this.store.dispatch(new ActorSearchActions.Adult(true));
+      this.manualSearch();
+    }
+  }
+
+  onPageChange(page: number): void {
+    this.store.dispatch(new ActorSearchActions.PageChange(page));
+  }
+
+  adultUnchecked() {
+    this.adultCounter = 0;
+    this.store.dispatch(new ActorSearchActions.Adult(false));
+    this.manualSearch();
+  }
+
+  protected readonly Math = Math;
+}
