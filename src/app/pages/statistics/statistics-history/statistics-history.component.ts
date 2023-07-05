dd3d2679851f593aceeb8ee13a770f2bc139a31a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-07-05 21:55:10 +0200

    feat: improve media loading in statistics history component
    - Improved the media loading functionality in the statistics history component.
    - Added logic to load additional information for TV episodes, movies, and TV shows.
    - Created maps to store the response data for episodes, movies, and TV shows.
    - Updated the HTML template to display the additional media information when available.
    - Fixed a bug where the media loading spinner was not being displayed correctly.

--- src/app/pages/statistics/statistics-history/statistics-history.component.ts
+++ src/app/pages/statistics/statistics-history/statistics-history.component.ts
@@ -2,12 +2,19 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { navigationRoot } from '../../../app-routing.module';
 import { streamingLinks } from '../../streaming/streaming-routing.module';
 import { Select } from '@ngxs/store';
-import { forkJoin, Observable, Subscription } from 'rxjs';
+import { forkJoin, map, Observable, Subscription, switchMap } from 'rxjs';
 import { AuthState } from '../../../core/auth/store/auth.state';
 import { EpisodeHistoryService } from '../../../feature/history/episode-history.service';
 import { MovieHistoryService } from '../../../feature/history/movie-history.service';
 import { HistoryModel } from '../../../shared/models/history.models';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import {
+  MovieResponse,
+  TvEpisodeResponse,
+  TvShowResponse,
+} from '../../../shared/models/media.models';
+import { MediaInfoService } from '../../../feature/media-info/media-info.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-statistics-history',
@@ -25,12 +32,18 @@ export class StatisticsHistoryComponent implements OnInit, OnDestroy {
   history: HistoryModel[] = [];
   historyLoading = false;
 
+  mediaLoading = false;
+  episodeResponseMap: Map<number, TvEpisodeResponse> = new Map();
+  movieResponseMap: Map<number, MovieResponse> = new Map();
+  tvShowsResponseMap: Map<number, TvShowResponse> = new Map();
+
   subscriptions: Subscription[] = [];
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     private episodeHistoryService: EpisodeHistoryService,
-    private movieHistoryService: MovieHistoryService
+    private movieHistoryService: MovieHistoryService,
+    private readonly mediaService: MediaInfoService
   ) {}
 
   ngOnInit(): void {
@@ -48,14 +61,68 @@ export class StatisticsHistoryComponent implements OnInit, OnDestroy {
         this.movieHistoryService.getMovieHistory(),
       ]).subscribe({
         next: ([episodeHistory, movieHistory]) => {
+          this.mediaLoading = true;
           this.history = [...episodeHistory, ...movieHistory].sort(
             (a, b) => b.viewedAt.getTime() - a.viewedAt.getTime()
           );
+          this.loadMedias();
         },
         complete: () => {
           this.historyLoading = false;
         },
       })
+    );
+  }
+
+  loadMedias() {
+    this.mediaLoading = true;
+    const episodesIds = this.history
+      .filter(history => history.type === 'tv-shows')
+      .map(history => history.mediaId);
+    const moviesIds = this.history
+      .filter(history => history.type === 'movies')
+      .map(history => history.mediaId);
+
+    const tvShowIds: Set<number> = new Set();
+    type MediaResponses = [
+      TvEpisodeResponse[],
+      MovieResponse[],
+      TvShowResponse[]
+    ];
+
+    this.subscriptions.push(
+      forkJoin([
+        this.mediaService.getTvShowEpisodesInfoByIds(episodesIds),
+        this.mediaService.getMoviesShortInfo(moviesIds),
+      ])
+        .pipe(
+          tap(([episodes]) => {
+            episodes.forEach(episode => {
+              tvShowIds.add(episode.tvShowId);
+            });
+          }),
+          switchMap(([episodes, movies]) => {
+            return this.mediaService
+              .getTvShowsShortInfo(Array.from(tvShowIds))
+              .pipe(
+                map(tvShows => [episodes, movies, tvShows] as MediaResponses)
+              );
+          })
+        )
+        .subscribe({
+          next: ([episodes, movies, tvShows]) => {
+            episodes.forEach(episode =>
+              this.episodeResponseMap.set(episode.id, episode)
+            );
+            movies.forEach(movie => this.movieResponseMap.set(movie.id, movie));
+            tvShows.forEach(tvShow =>
+              this.tvShowsResponseMap.set(tvShow.id, tvShow)
+            );
+          },
+          complete: () => {
+            this.mediaLoading = false;
+          },
+        })
     );
   }
 
